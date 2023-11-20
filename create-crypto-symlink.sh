2f540ba5440583eb7c60646425374916f66df34a
ashpool <ashpoolin@protonmail.com>
2023-11-19 23:15:41 -0700

    trying again with postinstall.js

--- create-crypto-symlink.sh
+++
@@ -1,6 +0,0 @@
-   #!/bin/bash
-
-   srcPath="./node_modules/crypto-browserify"
-   destPath="./src/components/crypto"
-
-   ln -s "$srcPath" "$destPath"
---
+++ postinstall.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, 'node_modules/crypto');
+const destPath = path.resolve(__dirname, 'src/components/crypto');
+
+fs.symlink(srcPath, destPath, 'dir', (err) => {
+  if (err) {
+    console.error('Failed to create symbolic link:', err);
+  } else {
+    console.log('Symbolic link created successfully.');
+  }
+});
+
+// import { symlink } from 'fs/promises';
+// import { resolve } from 'path';
+
+// const srcPath = resolve(__dirname, 'node_modules/crypto');
+// const destPath = resolve(__dirname, 'src/components/crypto');
+
+// symlink(srcPath, destPath, 'dir')
+//   .then(() => {
+//     console.log('Symbolic link created successfully.');
+//   })
+//   .catch((err) => {
+//     console.error('Failed to create symbolic link:', err);
+//   });
